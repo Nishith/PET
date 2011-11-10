888732ded88d3664cb4f30eaaca812cd56530b9f
Nishith Nand <mr.n.nand@gmail.com>
2011-11-09 16:13:56 -0800

    Changed the seed file to get the deliverables from the deliverable_type instead of Project_phases

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 HistoricalData.delete_all
 (1..10).each do
   #High complexity
-  types = ProjectPhaseDeliverable.all.collect {|p| p.deliverable_type_id}
+  types = DeliverableType.all.collect {|p| p.id}
   types.each do |t|
     rate = 5 + rand(5)
     size = 100 + rand(500)
@@ -9,7 +9,7 @@ HistoricalData.delete_all
     HistoricalData.create!(:deliverable_type_id => t, :complexity => 3, :total_effort => effort, :production_rate => rate, :estimated_size => size)
   end
   #Medium complexity
-  types = ProjectPhaseDeliverable.all.collect {|p| p.deliverable_type_id}
+  types = DeliverableType.all.collect {|p| p.id}
   types.each do |t|
     rate = 3 + rand(5)
     size = 100 + rand(500)
@@ -17,7 +17,7 @@ HistoricalData.delete_all
     HistoricalData.create!(:deliverable_type_id => t, :complexity => 2, :total_effort => effort, :production_rate => rate, :estimated_size => size)
   end
   #Low complexity
-  types = ProjectPhaseDeliverable.all.collect {|p| p.deliverable_type_id}
+  types = DeliverableType.all.collect {|p| p.id}
   types.each do |t|
     rate = 1 + rand(5)
     size = 100 + rand(500)
