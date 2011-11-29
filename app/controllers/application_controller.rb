afa5de72576d790dffb6959ec99089a73d23f795
Sheng-Hao Tsao <sheng-hao.tsao@sv.cmu.edu>
2011-11-28 20:46:43 -0800

    finished context-aware sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :load_sidebar
+
+  def load_sidebar
+    if current_user.nil?
+      @user_type = "not login"
+    else
+      @user_type = current_user.role
+    end
+  end
+
   # Handles the exception the CanCan gem generates when an unauthorized access is attempted.
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = exception.message
